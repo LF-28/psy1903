ad44ceb5389ba6846617c636735a5baa36318998
Eliza Fried <efried@college.harvard.edu>
2025-09-09 16:51:59 -0400

    added timed feature to mrt.js experiment

--- web/week2/mrt.js
+++ web/week2/mrt.js
@@ -1,16 +1,28 @@
 alert("In this experiment we will measure your response time. You will be shown a series of simple math equations. Answer these equations as quickly and accurately as you can."
 
 )
+let start1 = Date.now()
 let answer1 = prompt('What is 1+1');
 console.log(answer1);
+let end1 = Date.now()
+responseTime1 = (end1 - start1) / 1000;
 
-alert("you answered " + answer1);
+alert("you answered " + answer1 + "in " + responseTime1 + "seconds");
 
+let start2 = Date.now()
 let answer2 = prompt('What is 2+2');
 console.log(answer2);
-alert("you answered " + answer2);
+let end2 = Date.now()
+responseTime2 = (end2 - start2) / 1000;
 
-let answer3 = prompt('What is 1+1');
+alert("you answered " + answer2 + "in " + responseTime2 + "seconds");
+
+let start3 = Date.now()
+let answer3 = prompt('What is 3+3');
 console.log(answer3);
-alert("you answered " + answer3);
+let end3 = Date.now()
+responseTime3 = (end3 - start3) / 1000;
+
+alert("you answered " + answer3 + "in " + responseTime3 + "seconds");
+
 
