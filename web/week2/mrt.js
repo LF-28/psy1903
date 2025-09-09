b92fd72abfd902b911353c808bb814a403d550f2
Eliza Fried <efried@college.harvard.edu>
2025-09-09 17:07:17 -0400

    fixed mrt.js experiment randomized numbers

--- web/week2/mrt.js
+++ web/week2/mrt.js
@@ -1,8 +1,10 @@
 alert("In this experiment we will measure your response time. You will be shown a series of simple math equations. Answer these equations as quickly and accurately as you can."
 
 )
+let math1 = Math.floor(Math.random() * 10) + 1
+let math2 = Math.floor(Math.random() * 10) + 1
 let start1 = Date.now()
-let answer1 = prompt('What is ' + Math.floor(Math.random() * 10) + 1 + '+' + Math.floor(Math.random() * 10) + 1);
+let answer1 = prompt('What is ' + math1 + '+' + math2);
 console.log(answer1);
 let end1 = Date.now()
 responseTime1 = (end1 - start1) / 1000;
@@ -10,7 +12,9 @@ responseTime1 = (end1 - start1) / 1000;
 alert("you answered " + answer1 + "in " + responseTime1 + "seconds");
 
 let start2 = Date.now()
-let answer2 = prompt('What is ' + Math.floor(Math.random() * 10) + 1 + '+' + Math.floor(Math.random() * 10) + 1);
+math1 = Math.floor(Math.random() * 10) + 1
+math2 = Math.floor(Math.random() * 10) + 1
+let answer2 = prompt('What is ' + math1 + '+' + math2);
 console.log(answer2);
 let end2 = Date.now()
 responseTime2 = (end2 - start2) / 1000;
@@ -18,11 +22,14 @@ responseTime2 = (end2 - start2) / 1000;
 alert("you answered " + answer2 + "in " + responseTime2 + "seconds");
 
 let start3 = Date.now()
-let answer3 = prompt('What is ' + Math.floor(Math.random() * 10) + 1 + '+' + Math.floor(Math.random() * 10) + 1);
+math1 = Math.floor(Math.random() * 10) + 1
+math2 = Math.floor(Math.random() * 10) + 1
+let answer3 = prompt('What is ' + math1 + '+' + math2);
 console.log(answer3);
 let end3 = Date.now()
 responseTime3 = (end3 - start3) / 1000;
 
 alert("you answered " + answer3 + "in " + responseTime3 + "seconds");
+
 
 
