efa7489550b0ad8928072c5cae735070c02f4c69
Eliza Fried <efried@college.harvard.edu>
2025-09-16 16:56:04 -0400

    Q10 Math Experiment with Feedback

--- web/week-3-task-set/app.js
+++ web/week-3-task-set/app.js
@@ -67,9 +67,55 @@ else (age >= 18) {
 } */
 
 //Display if even or odd numbered entered
-let number = prompt("Please enter a whole number");
+/* let number = prompt("Please enter a whole number");
 if (number % 2 == 0) {
     alert("The number you entered was even")
 } else {
     alert("The number you entered is odd.")
-}; 
+};  */
+
+//Adding correct/incorrect to Math quiz
+alert("In this experiment we will measure your response time. You will be shown a series of simple math equations. Answer these equations as quickly and accurately as you can."
+
+)
+let math1 = Math.floor(Math.random() * 10) + 1
+let math2 = Math.floor(Math.random() * 10) + 1
+let start1 = Date.now()
+let answer1 = prompt('What is ' + math1 + '+' + math2);
+console.log(answer1);
+let end1 = Date.now()
+responseTime1 = (end1 - start1) / 1000;
+let correctAnswer = math1 + math2
+
+if (answer1 == correctAnswer) {
+    alert("you answered " + answer1 + "in " + responseTime1 + "seconds. You are CORRECT")
+}
+else { alert("you answered " + answer1 + "in " + responseTime1 + "seconds. You are INCORRECT") };
+
+let start2 = Date.now()
+math1 = Math.floor(Math.random() * 10) + 1
+math2 = Math.floor(Math.random() * 10) + 1
+correctAnswer = math1 + math2
+let answer2 = prompt('What is ' + math1 + '+' + math2);
+console.log(answer2);
+let end2 = Date.now()
+responseTime2 = (end2 - start2) / 1000;
+if (answer2 == correctAnswer) {
+    alert("you answered " + answer2 + "in " + responseTime2 + "seconds. You are CORRECT.")
+}
+else { alert("you answered " + answer2 + "in " + responseTime2 + "seconds. You are INCORRECT.") };
+
+
+let start3 = Date.now()
+math1 = Math.floor(Math.random() * 10) + 1
+math2 = Math.floor(Math.random() * 10) + 1
+let answer3 = prompt('What is ' + math1 + '+' + math2);
+correctAnswer = math1 + math2
+console.log(answer3);
+let end3 = Date.now()
+responseTime3 = (end3 - start3) / 1000;
+if (answer3 == correctAnswer) {
+    alert("you answered " + answer3 + "in " + responseTime3 + "seconds. You are CORRECT.")
+}
+else { alert("you answered " + answer3 + "in " + responseTime3 + "seconds. You are INCORRECT") };
+
