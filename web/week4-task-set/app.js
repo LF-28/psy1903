83c499f9e0f57ff63a74caecaf46ecfbe9e4c72b
Eliza Fried <efried@college.harvard.edu>
2025-09-23 16:24:52 -0400

    convertTemp function

--- web/week4-task-set/app.js
+++ web/week4-task-set/app.js
@@ -1,6 +1,21 @@
-function celsiusToFahrenheit(celsius) {
+//Q2
+// function celsiusToFahrenheit(celsius) {
 
-    celsiusToFahrenheit = (celsius * 1.8) + 32
-    return celsiusToFahrenheit
-};
-console.log(celsiusToFahrenheit(10));
+//     celsiusToFahrenheit = (celsius * 1.8) + 32
+//     return celsiusToFahrenheit
+// };
+// console.log(celsiusToFahrenheit(10));
+
+//Q3
+
+function convertTemp(temp, convertTo) {
+    if (convertTo == 'c') {
+        let convertTemp = (temp - 32) / 1.8;
+        return convertTemp
+    } else {
+        (temp * 1.8) + 32
+        return convertTemp
+    }
+
+}
+
