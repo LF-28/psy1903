2931511bc17ff6bf6e71850b32f1ae59ffc94074
Eliza Fried <efried@college.harvard.edu>
2025-09-23 20:36:49 -0400

    Even/Odd Response Time Task

--- web/week4-task-set/app.js
+++ web/week4-task-set/app.js
@@ -63,22 +63,56 @@
 // console.log(getOddNumbers([12, 45, 10, 11, 61])); // Expected output: [45, 11, 61]
 
 //Q7
-function filterNumbers(numbers, evenOrOdd) {
-    let results = []
-    for (let number of numbers) {
-        if (evenOrOdd == 'even') {
-            if (number % 2 == 0) {
-                results.push(number);
-            }
-        } else if (evenOrOdd == 'odd') {
-            if (number % 2 != 0) {
-                results.push(number);
-            }
-        }
-    } return results
+// function filterNumbers(numbers, evenOrOdd) {
+//     let results = []
+//     for (let number of numbers) {
+//         if (evenOrOdd == 'even') {
+//             if (number % 2 == 0) {
+//                 results.push(number);
+//             }
+//         } else if (evenOrOdd == 'odd') {
+//             if (number % 2 != 0) {
+//                 results.push(number);
+//             }
+//         }
+//     } return results
+// }
+
+// console.log(filterNumbers([1, 2, 3, 4, 5], 'even')); // Expected output: [2, 4]
+// console.log(filterNumbers([1, 2, 3, 4, 5], 'odd')); // Expected output: [1, 3, 5]
+// console.log(filterNumbers([45, 10, 11, 61], 'even')); // Expected output: [10]
+// console.log(filterNumbers([45, 10, 11, 61], 'odd')); // Expected output: [45, 11, 61]
+
+
+//Q8
+alert("Welcome to the even/odd response time task. You are about to see a series of numbers. If the number you see is EVEN, type the letter 'e'.  If the number you see is odd, type the letter 'o'.  Please answer as quickly and accurately as possible.")
+results = []
+for (let i = 0; i < 5; i++) {
+    let num1 = getRandomNumber(1, 10);
+    let start = Date.now();
+    let response = prompt(`Is ${num1} even or odd? Press e for even and o for odd`);
+    let end = Date.now();
+    let responseTime = (end - start) / 1000;
+    let expected = ""
+    if (num1 % 2 == 0) {
+        expected = 'e';
+    } else {
+        expected = 'o';
+    }
+    let correct = (response == expected);
+    trial = {
+        number: num1,
+        response: response,
+        correct: correct,
+        responseTime: responseTime,
+
+    };
+    results.push(trial);
 }
 
-console.log(filterNumbers([1, 2, 3, 4, 5], 'even')); // Expected output: [2, 4]
-console.log(filterNumbers([1, 2, 3, 4, 5], 'odd')); // Expected output: [1, 3, 5]
-console.log(filterNumbers([45, 10, 11, 61], 'even')); // Expected output: [10]
-console.log(filterNumbers([45, 10, 11, 61], 'odd')); // Expected output: [45, 11, 61]
+function getRandomNumber(min, max) {
+    let randomNumber = Math.floor(Math.random() * max) + min;
+    return randomNumber;
+};
+
+console.log(results); 
