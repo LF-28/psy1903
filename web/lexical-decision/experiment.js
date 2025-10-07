66ae2f9e468e881016562dbf92ad313e158cf2cc
Eliza Fried <efried@college.harvard.edu>
2025-10-07 15:11:40 -0400

    week 6 demo

--- web/lexical-decision/experiment.js
+++ web/lexical-decision/experiment.js
@@ -1,29 +1,21 @@
-let jsPsych = initJsPsych(); //how all jsPsych code starts
+let jsPsych = initJsPsych();
 
 let timeline = [];
 
-//Welcome
+// let conditions = [...redacted for brevity - see above...]
+
 let welcomeTrial = {
     type: jsPsychHtmlKeyboardResponse,
     stimulus: `
     <h1>Welcome to the Lexical Decision Task!</h1> 
     <p>In this experiment, you will be shown a series of characters and asked to categorize whether the characters make up a word or not.</p>
     <p>There are three parts to this experiment.</p>
-    <p>Press SPACE to begin the first part.</p>
+    <p class = 'instructions'>Press SPACE to begin the first part.</p>
     `,
-
     choices: [' '],
-
 };
 timeline.push(welcomeTrial);
-
-
-//Showed word or pseudo word (on repeat)
-
-
-
-// Shuffle the conditions
-conditions = jsPsych.randomization.repeat(conditions, 1);
+//Shuffle conditions
 for (let block of conditions) {
 
     let blockIntroTrial = {
@@ -67,6 +59,59 @@ for (let block of conditions) {
     }
 }
 
+
+//saving results
+let resultsTrial = {
+    type: jsPsychHtmlKeyboardResponse,
+    choices: ['NO KEYS'],
+    async: false,
+    stimulus: `
+        <h1>Please wait...</h1>
+        <p>We are saving the results of your inputs.</p>
+        `,
+    on_start: function () {
+        //  ⭐ Update the following three values as appropriate ⭐
+        let prefix = 'lexical-decision';
+        let dataPipeExperimentId = 'your-experiment-id-here';
+        let forceOSFSave = false;
+
+        // Filter and retrieve results as CSV data
+        let results = jsPsych.data
+            .get()
+            .filter({ collect: true })
+            .ignore(['stimulus', 'trial_type', 'plugin_version', 'collect'])
+            .csv();
+
+        // Generate a participant ID based on the current timestamp
+        let participantId = new Date().toISOString().replace(/T/, '-').replace(/\..+/, '').replace(/:/g, '-');
+
+        // Dynamically determine if the experiment is currently running locally or on production
+        let isLocalHost = window.location.href.includes('localhost');
+
+        let destination = '/save';
+        if (!isLocalHost || forceOSFSave) {
+            destination = 'https://pipe.jspsych.org/api/data/';
+        }
+
+        // Send the results to our saving end point
+        fetch(destination, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: '*/*',
+            },
+            body: JSON.stringify({
+                experimentID: dataPipeExperimentId,
+                filename: prefix + '-' + participantId + '.csv',
+                data: results,
+            }),
+        }).then(data => {
+            console.log(data);
+            jsPsych.finishTrial();
+        })
+    }
+}
+timeline.push(resultsTrial);
 
 //Debrief
 let debriefTrial = {
